5bded6579aa6c416ac6d6bb41725d826a3cb1f71
Jackson Bowen <jacksonbowenwork@gmail.com>
2020-01-18 17:54:01 -0500

    initial commit

---
+++ BlazorDemoApp/DataAccessLibrary/SqlDataAccess.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace DataAccessLibrary
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+        public string ConnectionStringName { get; set; } = "Default";
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<List<T>> LoadData<T, U>(string sql, U paramaters)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var data = await connection.QueryAsync<T>(sql, paramaters);
+
+                return data.ToList();
+            }
+        }
+        public async Task SaveData<T>(string sql, T parameters)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.ExecuteAsync(sql, parameters);
+            }
+        }
+    }
+}
